fdc027194f663c237207fd7260ac01bc39fe6f2b
Roman Fiodorov <xfilosofx@gmail.com>
2017-11-29 14:57:01 +0200

    Added facebook authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,64 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http';
-import { TranslateModule } from '@ngx-translate/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {TranslateModule} from '@ngx-translate/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
 
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
-import { HomeModule } from './home/home.module';
-import { AboutModule } from './about/about.module';
-import { LoginModule } from './login/login.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+
+import {CoreModule} from './core/core.module';
+import {SharedModule} from './shared/shared.module';
+import {HomeModule} from './home/home.module';
+import {AboutModule} from './about/about.module';
+import {LoginModule} from './login/login.module';
+
+import { SocialLoginModule } from 'angular4-social-login';
+import {GoogleLoginProvider, FacebookLoginProvider, AuthServiceConfig} from 'angular4-social-login';
+
+let config = new AuthServiceConfig([
+    // {
+    //     id: GoogleLoginProvider.PROVIDER_ID,
+    //     provider: new GoogleLoginProvider('453019100708-1aqms6ktq76445rbsnv8i7fcouetjqr0.apps.googleusercontent.com')
+    // },
+    {
+        id: FacebookLoginProvider.PROVIDER_ID,
+        provider: new FacebookLoginProvider('370860263035094')
+    }
+]);
+
+export function provideConfig() {
+    return config;
+}
 
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    HttpClientModule,
-    TranslateModule.forRoot(),
-    BrowserAnimationsModule,
-    MaterialModule,
-    CoreModule,
-    SharedModule,
-    HomeModule,
-    AboutModule,
-    LoginModule,
-    AppRoutingModule
-  ],
-  declarations: [AppComponent],
-  providers: [
-  ],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        HttpClientModule,
+        TranslateModule.forRoot(),
+        BrowserAnimationsModule,
+        MaterialModule,
+        CoreModule,
+        SharedModule,
+        HomeModule,
+        AboutModule,
+        LoginModule,
+        SocialLoginModule,
+        AppRoutingModule
+    ],
+    declarations: [AppComponent],
+    providers: [{
+        provide: AuthServiceConfig,
+        useFactory: provideConfig
+    }],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,70 +1,94 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { finalize } from 'rxjs/operators';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {finalize} from 'rxjs/operators';
 
-import { environment } from '../../environments/environment';
-import { Logger } from '../core/logger.service';
-import { I18nService } from '../core/i18n.service';
-import { AuthenticationService } from '../core/authentication/authentication.service';
+import {environment} from '../../environments/environment';
+import {Logger} from '../core/logger.service';
+import {I18nService} from '../core/i18n.service';
+import {AuthenticationService} from '../core/authentication/authentication.service';
+import {AuthService, FacebookLoginProvider, SocialUser} from 'angular4-social-login';
 
 const log = new Logger('Login');
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  version: string = environment.version;
-  error: string = null;
-  loginForm: FormGroup;
-  isLoading = false;
+    private user: SocialUser;
 
-  constructor(private router: Router,
-              private formBuilder: FormBuilder,
-              private i18nService: I18nService,
-              private authenticationService: AuthenticationService) {
-    this.createForm();
-  }
+    version: string = environment.version;
+    error: string = null;
+    loginForm: FormGroup;
+    isLoading = false;
 
-  ngOnInit() { }
+    constructor(private router: Router,
+                private formBuilder: FormBuilder,
+                private i18nService: I18nService,
+                private authService: AuthService,
+                private authenticationService: AuthenticationService) {
+        this.createForm();
+    }
 
-  login() {
-    this.isLoading = true;
-    this.authenticationService.login(this.loginForm.value)
-      .pipe(finalize(() => {
-        this.loginForm.markAsPristine();
-        this.isLoading = false;
-      }))
-      .subscribe(credentials => {
-        log.debug(`${credentials.username} successfully logged in`);
-        this.router.navigate(['/'], { replaceUrl: true });
-      }, error => {
-        log.debug(`Login error: ${error}`);
-        this.error = error;
-      });
-  }
+    ngOnInit() {
+        this.authService.authState.subscribe((user: SocialUser) => {
+            this.user = user;
+        });
+    }
 
-  setLanguage(language: string) {
-    this.i18nService.language = language;
-  }
+    login() {
+        this.isLoading = true;
+        console.log('Why????');
+        this.authenticationService.login(this.loginForm.value)
+            .pipe(finalize(() => {
+                this.loginForm.markAsPristine();
+                this.isLoading = false;
+            }))
+            .subscribe(credentials => {
+                log.debug(`${credentials.username} successfully logged in`);
+                this.router.navigate(['/'], {replaceUrl: true});
+            }, error => {
+                log.debug(`Login error: ${error}`);
+                this.error = error;
+            });
+    }
 
-  get currentLanguage(): string {
-    return this.i18nService.language;
-  }
+    signInWithGoogle(): void {
+        // this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    }
 
-  get languages(): string[] {
-    return this.i18nService.supportedLanguages;
-  }
+    signInWithFB(): void {
+        this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(socialUser =>{
+            console.log(socialUser.authToken);
+        });
 
-  private createForm() {
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required],
-      remember: true
-    });
-  }
+    }
+
+    signOut(): void {
+        this.authService.signOut();
+    }
+
+    setLanguage(language: string) {
+        this.i18nService.language = language;
+    }
+
+    get currentLanguage(): string {
+        return this.i18nService.language;
+    }
+
+    get languages(): string[] {
+        return this.i18nService.supportedLanguages;
+    }
+
+    private createForm() {
+        this.loginForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            password: ['', Validators.required],
+            remember: true
+        });
+    }
 
 }
